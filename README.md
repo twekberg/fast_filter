37df3257203024e3dc160bbb1d797985ed22fdb5
Tom Ekberg <tekberg@uw.edu>
2019-08-27 11:43:57 -0700

    Added pagination in the form of < << page numbers >> >

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# fast_filter
+fast_filter
+===========
+
+Fast filtering for when you hare a large amount of data to display.
+
+Overview
+--------
+
 Based on Chris Smith's Lightning Fast Filtering in JavaScript (https://chrissmith.xyz/lightning-fast-filtering-in-javascript/). I added pagination.
+
+Detail
+------
+
+After cloning this repo, point your browser to fast.html. The
+Javascript and CSS are in the same directory. 
+
+The change was to add pagination which the original version didn't
+have.
+
---
+++ fast.js
@@ -0,0 +1,143 @@
+console.clear();
+
+var list = [],
+    filteredList = [],
+    maxDisplayLimit = 10,
+    page = 1,
+    n_pages = 0,
+    textInput = document.querySelector('.text-filter'),
+    displayList = document.querySelector('.list'),
+    countMessage = document.querySelector('.count-message'),
+    pagination = document.querySelector('.pagination');
+
+function generateDummyList(itemCount) {
+  if (!itemCount) return;
+    n_pages = Math.ceil(itemCount / maxDisplayLimit);
+  for (var i = 0; i < itemCount; i++) {
+    var item = {
+      name: Math.random().toString(36).substr(2, 10),
+      type: Math.random().toString(36).substr(2, 10),
+      category: Math.random().toString(36).substr(2, 10)
+    };
+    list.push(item);
+  }
+}
+
+function generateCountMessage() {
+  var msg = '',
+      matches = filteredList.length;
+  switch (true) {
+    case (matches === 0):
+      msg = 'No matches found';
+      break;
+    case (matches === 1):
+      msg = 'Showing 1 item';
+      break;
+    case (matches <= maxDisplayLimit):
+      msg = 'Showing ' + filteredList.length + ' items';
+      break;
+    default:
+      msg = 'Showing ' + maxDisplayLimit + ' of ' + matches + ' items';
+  }
+  countMessage.textContent = msg;
+}
+  
+function pageLink(page, display) {
+    // page - the page the user will go do
+    // display - the displayed part of the link
+    return '<a href="#" onclick="generateList(' + page + ');  return false;">' + display + '</a>';
+}
+
+function generatePagination() {
+    // Pagination is composed of 3 parts:
+    //   before - the < and << icons if they make sense, and numbers prior to the current page.
+    //   here - the current page number in bold.
+    //   after - page numbers after the current page and the >> and > icons if they make sense.
+    var before = '',
+	here = '',
+	after = '',
+	befores = [pageLink(page - 1, '&lt;'),
+		   pageLink(1,        '&Lt;')],
+	afters =  [pageLink(n_pages,  '&Gt;'),
+		   pageLink(page + 1, '&gt;')];
+    if (page > 1) {
+	if (page > 2) {
+	    before = befores.join(' ');
+	} else {
+	    before = befores[0];
+	}
+	before = before + ' ';
+    }
+    for(var i = page-3; i<page; i++) {
+	if (i >= 1) {
+	    before = before + i + ' ';
+	}
+    }
+    here = '<span style="font-weight: bold">' + page + '</span>';
+
+    for(var i = page+1 ; i<=page+3; i++) {
+	if (i <=  n_pages) {
+	    after = after + ' ' + i;
+	}
+    }
+    if (page < n_pages) {
+	after = after + ' ';
+	if (page + 1 < n_pages) {
+	    after = after + afters.join(' ');
+	} else {
+	    after = after + afters[afters.length - 1];
+	}
+    }
+    pagination.innerHTML = before + here + after;
+}
+
+
+function generateListItem(item) {
+    var li = document.createElement('li'),
+          spanName = document.createElement('span'),
+          spanType = document.createElement('span'),
+          spanCategory = document.createElement('span');
+      spanName.classList.add('name');
+      spanType.classList.add('type');
+      spanCategory.classList.add('category');
+      spanName.textContent = item.name;
+      spanType.textContent = item.type;
+      spanCategory.textContent = item.category;
+      li.appendChild(spanName);
+      li.appendChild(spanType);
+      li.appendChild(spanCategory); 
+  return li;
+}
+
+function generateList(thisPage) {
+  var frag = document.createDocumentFragment();
+  page = thisPage
+  for (var i = 0; i < filteredList.length; i++) {
+    if (i < maxDisplayLimit) {
+      var item = filteredList[i + thisPage - 1],
+          li = generateListItem(item);
+      frag.appendChild(li);
+    }
+    else break;
+  }
+  displayList.innerHTML = '';
+  displayList.appendChild(frag);
+  generateCountMessage();
+  generatePagination();
+}
+
+function textMatch(item) {
+  var searchTerm = textInput.value.toLowerCase(),
+      itemText = (item.name + item.type + item.category).toLowerCase();
+  return itemText.indexOf(searchTerm) !== -1;
+}
+
+function getFilteredItems() {
+  filteredList = list.filter(textMatch);
+  generateList(page);
+}
+
+textInput.addEventListener('keyup', getFilteredItems);
+
+generateDummyList(100000);
+getFilteredItems();
